400892b5c3a50608631e72a1a47c179648bc6601
Marcos Rodriguez Fernandez <uo299751@uniovi.es>
2025-02-01 16:59:59 +0100

    Adding First Java file

---
+++ algstudent.s0/JavaA1.java
@@ -0,0 +1,62 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaA1 {
+	
+	public static void main(String[] args) {
+		int n = 10000;
+		
+		for (int i=0; i<7; i++) {
+			long t1= System.currentTimeMillis();
+			List<Integer> primes = listadoPrimos(n);
+			long t2 = System.currentTimeMillis();
+			
+            System.out.println("n = " + n + " *** time = " + (t2 - t1) + " milliseconds");
+            n *=2;
+		}
+	}
+
+	/**
+	 * Calculates and returns all primes up to n.
+	 * @return
+	 */
+	private static List<Integer> listadoPrimos(int n)
+	{
+		List<Integer> primes = new ArrayList<Integer>();
+	    for (int i=2; i<=n; i++)
+	    {
+	    	if (primoA1(i))
+	    	{
+	            primes.add(i);
+	        }
+	    }
+	  
+	    return primes;
+	}
+	    
+	
+	/**
+	 * Returns whether m is prime or not.
+	 * @param m
+	 * @return
+	 */
+	private static boolean primoA1(int m)
+	{
+		boolean p = true;
+		
+		for(int i = 2; i<m; i++)
+		{
+			  if( m % i == 0)
+			  {
+				  p = false;
+			  }
+		           
+		}
+	    return p;
+	      
+	}
+	    
+
+}
