0d05a0a7395b23a19562a050d2aed8fd71bb8f9f
Marcos Rodriguez Fernandez <uo299751@uniovi.es>
2025-02-01 17:01:00 +0100

    Adding Third Java file

---
+++ algstudent.s0/JavaA3.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaA3 {
+	public static void main(String[] args) {
+        int n = 10000;
+
+        for (int casos = 0; casos < 7; casos++) {  
+            long t1 = System.currentTimeMillis();  
+            List<Integer> primes = listadoPrimos(n);  
+            long t2 = System.currentTimeMillis();  
+
+            System.out.println("n = " + n + " *** time = " + (t2 - t1) + " milliseconds");
+
+            n *= 2; 
+        }
+    }
+
+    public static List<Integer> listadoPrimos(int n) {
+        List<Integer> primes = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (primoA3(i))
+            {  
+                primes.add(i);
+            }
+        }
+
+        return primes;
+    }
+
+    public static boolean primoA3(int m) {
+        for (int i = 2; i <= m / 2; i++)
+        {
+            if (m % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
